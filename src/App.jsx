f06d619bf8f11053426bc1b8ecab9bff77ffc57a
saurabh bhardwaj <saurabhpandat27@gmail.com>
2025-05-06 14:03:00 +0530

    initial done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Booking from './assets/Booking'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Booking/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Booking.jsx
@@ -0,0 +1,229 @@
+import React from 'react';
+
+
+const FlightCard = () => {
+  return (
+    <>
+    
+    <div className='container'>
+    <span className='cleartrip-logo'>
+        <img 
+        style={{
+            width:'150px'
+        }} src="https://etimg.etb2bimg.com/photo/94049186.cms" alt="" />
+    </span>
+    <div className='main-div'>
+        <div style={{
+            display:'flex',
+            flexDirection:"column",
+            float:'left',
+            
+        }}>
+           
+        <span
+        style={{
+            display:'flex',
+            flexDirection:'column',
+            float:'left',
+            display:'inline-block',
+            width:'100px',
+            position:'relative',
+            right:'50px',
+            marginRight:'10px'
+            
+            
+        }}>
+        <button className='btn'>Flights</button>
+    <button className='btn'>Hotels</button>
+    <button className='btn'>Buses</button>
+        </span>
+        </div>
+   
+    <div className="flight-card">
+        
+      <div className="image-section">
+        <img
+          src="https://blog.air.irctc.co.in/wp-content/uploads/2023/04/aeroplane-ticket-booking.jpg"
+          alt="Flight Offer"
+        />
+        <div className="overlay">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/UPI-Logo-vector.svg/2560px-UPI-Logo-vector.svg.png"
+            alt="UPI Logo"
+            className="upi-logo"
+          />
+          <h3>Get up to ₹5,000 off</h3>
+          <p>on Flights with UPI Payments!</p>
+          <small>on Domestic & International Flights</small>
+        </div>
+      </div>
+      <div className="card-actions">
+        <button className="ctupi-btn">
+          CTUPI
+          <img
+            src="https://png.pngtree.com/png-clipart/20190619/original/pngtree-vector-copy-icon-png-image_4013516.jpg"
+            alt="copy"
+          />
+        </button>
+        <button className="book-btn">BOOK NOW</button>
+      </div>
+      
+      
+    </div>
+    <div className="flight-card">
+      <div className="image-section">
+        <img
+          src="https://blog.air.irctc.co.in/wp-content/uploads/2023/04/aeroplane-ticket-booking.jpg"
+          alt="Flight Offer"
+        />
+        <div className="overlay">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/UPI-Logo-vector.svg/2560px-UPI-Logo-vector.svg.png"
+            alt="UPI Logo"
+            className="upi-logo"
+          />
+          <h3>Get up to ₹5,000 off</h3>
+          <p>on Flights with UPI Payments!</p>
+          <small>on Domestic & International Flights</small>
+        </div>
+      </div>
+      <div className="card-actions">
+        <button className="ctupi-btn">
+          CTUPI
+          <img
+            src="https://png.pngtree.com/png-clipart/20190619/original/pngtree-vector-copy-icon-png-image_4013516.jpg"
+            alt="copy"
+          />
+        </button>
+        <button className="book-btn">BOOK NOW</button>
+      </div>
+      
+      
+    </div>
+    <div className="flight-card">
+      <div className="image-section">
+        <img
+          src="https://blog.air.irctc.co.in/wp-content/uploads/2023/04/aeroplane-ticket-booking.jpg"
+          alt="Flight Offer"
+        />
+        <div className="overlay">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/UPI-Logo-vector.svg/2560px-UPI-Logo-vector.svg.png"
+            alt="UPI Logo"
+            className="upi-logo"
+          />
+          <h3>Get up to ₹5,000 off</h3>
+          <p>on Flights with UPI Payments!</p>
+          <small>on Domestic & International Flights</small>
+        </div>
+      </div>
+      <div className="card-actions">
+        <button className="ctupi-btn">
+          CTUPI
+          <img
+            src="https://png.pngtree.com/png-clipart/20190619/original/pngtree-vector-copy-icon-png-image_4013516.jpg"
+            alt="copy"
+          />
+        </button>
+        <button className="book-btn">BOOK NOW</button>
+      </div>
+      
+      
+    </div>
+    <div className="flight-card">
+      <div className="image-section">
+        <img
+          src="https://blog.air.irctc.co.in/wp-content/uploads/2023/04/aeroplane-ticket-booking.jpg"
+          alt="Flight Offer"
+        />
+        <div className="overlay">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/UPI-Logo-vector.svg/2560px-UPI-Logo-vector.svg.png"
+            alt="UPI Logo"
+            className="upi-logo"
+          />
+          <h3>Get up to ₹5,000 off</h3>
+          <p>on Flights with UPI Payments!</p>
+          <small>on Domestic & International Flights</small>
+        </div>
+      </div>
+      <div className="card-actions">
+        <button className="ctupi-btn">
+          CTUPI
+          <img
+            src="https://png.pngtree.com/png-clipart/20190619/original/pngtree-vector-copy-icon-png-image_4013516.jpg"
+            alt="copy"
+          />
+        </button>
+        <button className="book-btn">BOOK NOW</button>
+      </div>
+      
+      
+    </div>
+    <div className="flight-card">
+      <div className="image-section">
+        <img
+          src="https://blog.air.irctc.co.in/wp-content/uploads/2023/04/aeroplane-ticket-booking.jpg"
+          alt="Flight Offer"
+        />
+        <div className="overlay">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/UPI-Logo-vector.svg/2560px-UPI-Logo-vector.svg.png"
+            alt="UPI Logo"
+            className="upi-logo"
+          />
+          <h3>Get up to ₹5,000 off</h3>
+          <p>on Flights with UPI Payments!</p>
+          <small>on Domestic & International Flights</small>
+        </div>
+      </div>
+      <div className="card-actions">
+        <button className="ctupi-btn">
+          CTUPI
+          <img
+            src="https://png.pngtree.com/png-clipart/20190619/original/pngtree-vector-copy-icon-png-image_4013516.jpg"
+            alt="copy"
+          />
+        </button>
+        <button className="book-btn">BOOK NOW</button>
+      </div>
+      
+      
+    </div>
+    <div className="flight-card">
+      <div className="image-section">
+        <img
+          src="https://blog.air.irctc.co.in/wp-content/uploads/2023/04/aeroplane-ticket-booking.jpg"
+          alt="Flight Offer"
+        />
+        <div className="overlay">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/UPI-Logo-vector.svg/2560px-UPI-Logo-vector.svg.png"
+            alt="UPI Logo"
+            className="upi-logo"
+          />
+          <h3>Get up to ₹5,000 off</h3>
+          <p>on Flights with UPI Payments!</p>
+          <small>on Domestic & International Flights</small>
+        </div>
+      </div>
+      <div className="card-actions">
+        <button className="ctupi-btn">
+          CTUPI
+          <img
+            src="https://png.pngtree.com/png-clipart/20190619/original/pngtree-vector-copy-icon-png-image_4013516.jpg"
+            alt="copy"
+          />
+        </button>
+        <button className="book-btn">BOOK NOW</button>
+      </div>
+      
+      
+    </div>
+    </div>
+    </div>
+    </>
+  );
+};
+
+export default FlightCard;
